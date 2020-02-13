d5d3ff772448a6c938959a7158498dec4f3db705
rumothy <rumothy@yahoo.com>
2020-02-13 09:47:11 -0500

    Updates

---
+++ precache-manifest.a33c9950c1ba90b2af12745f90eee6a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b075bd50f85e278fda334da35a79444a",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f7c9d779132708ad3510",
+    "url": "/clickygame/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc5e2e816fbad40007cc",
+    "url": "/clickygame/static/js/2.1f831936.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clickygame/static/js/2.1f831936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c9d779132708ad3510",
+    "url": "/clickygame/static/js/main.1fef998b.chunk.js"
+  },
+  {
+    "revision": "4d95e025c74c73140b85",
+    "url": "/clickygame/static/js/runtime-main.2999711a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
