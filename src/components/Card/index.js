a7389cf531e49d294ad8c4c0ec92bf1033e04d0e
rumothy <rumothy@yahoo.com>
2020-02-13 10:09:54 -0500

    Added Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Card extends React.Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <h1>Hello Card</h1>
+        );
+    }
+}
+
+export default Card;
